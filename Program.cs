73ac32f640978e8b5043de8d6644f16ec68240f2
namubar <mubarakshin.nail@tatar.ru>
2022-11-07 21:18:46 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter an integer ");
+int n = int.Parse(Console.ReadLine()!);
+int res = 0;
+int sumDigit(int n)
+{
+    while (n > 0)
+    {
+        int dig = n % 10;
+        res = res + dig;
+        n = n / 10;
+    }
+    return res;
+}
+sumDigit (n);
+
+Console.WriteLine("сумма цифр " + res);
+
+
